5d430fb1273445ba7d1d18cb3f4563e40361c102
RogueKatoz <adam.katic1@gmail.com>
2026-05-26 13:54:41 +0930

    Get firstName, lastName, and birthday from file and create new patient from it.

--- PatientSystem/PatientFileLoader.cpp
+++ PatientSystem/PatientFileLoader.cpp
@@ -18,7 +18,29 @@ std::vector<Patient*> PatientFileLoader::loadPatientFile(const std::string& file
 
     std::ifstream inFile(file);
     if (inFile.is_open()) {
-        // TODO: load your file here
+        std::string line;
+
+        while (std::getline(inFile, line))
+        {
+            //Initialise starting fields and grab them.
+            std:string uid, firstName, lastName, birthday;
+            std::stringstream lineStream(line);
+            char delimiter = '|';
+
+            std::getline(lineStream, uid, delimiter);
+            std::getline(lineStream, lastName, delimiter);
+            std::getline(lineStream, firstName, delimiter);
+            std::getline(lineStream, birthday, delimiter);
+
+            // Get time from birthday, taken from database loader.
+            std::tm t{ };
+            std::istringstream ss(birthday);
+            ss >> std::get_time(&t, "%d-%m-%Y");
+
+            // Make new patient from starting fields.
+			Patient* p = new Patient(firstName, lastName, t);
+
+        }
     }
 
     return patients;
