cdd2997b3ce2eb55c9000d6b7e816c7fe89bbe81
RogueKatoz <adam.katic1@gmail.com>
2026-05-26 14:31:54 +0930

    Add diagnosis and vitals parsing for file loader.

--- PatientSystem/PatientFileLoader.cpp
+++ PatientSystem/PatientFileLoader.cpp
@@ -23,15 +23,15 @@ std::vector<Patient*> PatientFileLoader::loadPatientFile(const std::string& file
         while (std::getline(inFile, line))
         {
             //Initialise starting fields and grab them. uid is ignored as it is generated anyway.
-            std:string uid, fullName, birthday, diagnosisStr, vitalsStr;
+            std:string uid, fullName, birthday, diagnosis, vitals;
         
-            std::stringstream lineStream(line);
+            std::istringstream lineStream(line);
 
             std::getline(lineStream, uid, '|');
             std::getline(lineStream, fullName, '|');
             std::getline(lineStream, birthday, '|');
-            std::getline(lineStream, diagnosisStr, '|');
-            std::getline(lineStream, vitalsStr, '|');
+            std::getline(lineStream, diagnosis, '|');
+            std::getline(lineStream, vitals, '|');
 
             // Split full name into first and last name.
 		    std::string firstName, lastName;
@@ -44,12 +44,33 @@ std::vector<Patient*> PatientFileLoader::loadPatientFile(const std::string& file
             std::istringstream ss(birthday);
             ss >> std::get_time(&t, "%d-%m-%Y");
 
-            // Make new patient.
+            // Make new patient and add diagnosis.
 			Patient* p = new Patient(firstName, lastName, t);
+            p->addDiagnosis(diagnosis);
 
-            
-    
+            // Vitals records don't always exist.
+            if (vitals != "")
+            {
+                // Split vitals records by ; then split again to get individual levels.
+				std::istringstream vitalsStream(vitals);
+				std::string vitalsRecord;
+				while (std::getline(vitalsStream, vitalsRecord, ';'))
+				{
+					std::istringstream vitalsRecordStream(vitalsRecord);
+					std::string bt, bp, hr, rr;
+					std::getline(vitalsRecordStream, bt, ',');
+					std::getline(vitalsRecordStream, bp, ',');
+					std::getline(vitalsRecordStream, hr, ',');
+					std::getline(vitalsRecordStream, rr, ',');
+					
+                    //Add this record to the patient.
+                    Vitals* v = new Vitals(stof(bt), stoi(bp), stoi(hr), stoi(rr));
+                    p->addVitals(v);
+				}
+            }
 
+            //Push back patient into vector and go to next.
+            patients.push_back(p);
         }
     }
 
