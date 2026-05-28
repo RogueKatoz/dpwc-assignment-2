bf38c05ec984b6a73c61f5e38a24c8fa7edd0269
RogueKatoz <adam.katic1@gmail.com>
2026-05-28 10:15:38 +0930

    Construct header file for CompositePatientLoader

--- PatientSystem/CompositePatientLoader.h
+++ PatientSystem/CompositePatientLoader.h
@@ -1,1 +1,19 @@
 #pragma once
+
+#include "AbstractPatientDatabaseLoader.h"
+
+#include <memory>
+#include <vector>
+
+class CompositePatientLoader : public AbstractPatientDatabaseLoader {
+public:
+	CompositePatientLoader() = default;
+
+	void createLoader(std::unique_ptr<AbstractPatientDatabaseLoader> loader);
+	void initialiseConnection() override;
+	void loadPatients(std::vector<Patient*>& patientIn) override;
+	void closeConnection() override;
+
+private:
+	std::vector<std::unique_ptr<AbstractPatientDatabaseLoader>> m_loaders;
+};
