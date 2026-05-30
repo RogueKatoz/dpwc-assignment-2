af8065a529b4e844fa23943c69d92b2f2d2ba803
RogueKatoz <adam.katic1@gmail.com>
2026-05-30 09:51:59 +0930

    Create abstract interface for alert levels.

--- PatientSystem/AlertLevelStrategy.h
+++ PatientSystem/AlertLevelStrategy.h
@@ -1,1 +1,15 @@
 #pragma once
+
+#include "PatientAlertLevels.h"
+
+// forward declare, avoid circular dependencies
+class Patient;
+class Vitals;
+
+class AlertLevelStrategy {
+public:
+	virtual ~AlertLevelStrategy() = default;
+
+	// Needs current vitals and patient age information.
+	virtual AlertLevel calculateAlertLevel(const Patient & patient, const Vitals & vitals) const = 0;
+};
